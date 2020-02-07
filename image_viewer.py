23218a6836c9f913e45f7caee22163bb74e32f78
l-nguyen <l-nguyen@dimage.co.jp>
2020-02-07 14:36:07 +0900

    exchange code to participate class

---
+++ image_viewer.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5.QtCore import pyqtSignal
+from show_video import ShowVideo
+
+class ImageViewer(QtWidgets.QWidget):
+    def __init__(self, parent = None):
+        super(ImageViewer, self).__init__(parent)
+        self.image = QtGui.QImage()
+        self.setAttribute(QtCore.Qt.WA_OpaquePaintEvent)
+ 
+ 
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(0,0, self.image)
+        self.image = QtGui.QImage()
+ 
+    def initUI(self):
+        self.setWindowTitle('Test')
+ 
+    @QtCore.pyqtSlot(QtGui.QImage)
+    def setImage(self, image):
+        if image.isNull():
+            print("Viewer Dropped frame!")
+ 
+        self.image = image
+        if image.size() != self.size():
+            self.setFixedSize(image.size())
+        self.update()
---
+++ push_button.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import sys
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5.QtCore import pyqtSignal
+
+class PushBut(QtWidgets.QPushButton):
+    def __init__(self, background_url, parent=None):
+        super(PushBut1, self).__init__(parent)
+        self.setMouseTracking(True)
+        self.setStyleSheet("margin: 1px; padding: 1px; border-style: solid; border-radius: 3px; border-width: 0.5px; border-color: rgba(127,127,255,255);")
+        self.background_url = background_url
+        self.setIcon(QtGui.QIcon(background_url))
+        self.setFixedSize(72, 72)
+        self.setIconSize(QtCore.QSize(70, 70))
+
+    def enterEvent(self, event):
+        if self.isEnabled() is True:
+            self.setStyleSheet("margin: 1px; padding: 1px; border-style: solid; border-radius: 3px; border-width: 0.5px; border-color: rgba(0,230,255,255);")
+            self.setIcon(QtGui.QIcon(self.background_url))
+            self.setFixedSize(72, 72)
+            self.setIconSize(QtCore.QSize(70, 70))
+
+        if self.isEnabled() is False:
+            self.setStyleSheet("margin: 1px; padding: 1px; border-style: solid; border-radius: 3px; border-width: 0.5px; border-color: rgba(127,127,255,255);")
+            self.setIcon(QtGui.QIcon(self.background_url))
+            self.setFixedSize(72, 72)
+            self.setIconSize(QtCore.QSize(70, 70))
+
+    def leaveEvent(self, event):
+        self.setStyleSheet("margin: 1px; padding: 1px; border-style: solid; border-radius: 3px; border-width: 0.5px; border-color: rgba(127,127,255,255);")
+        self.setIcon(QtGui.QIcon(self.background_url))
+        self.setFixedSize(72, 72)
+        self.setIconSize(QtCore.QSize(70, 70))
